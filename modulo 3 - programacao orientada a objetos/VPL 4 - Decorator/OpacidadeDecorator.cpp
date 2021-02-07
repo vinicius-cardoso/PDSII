eb96b557313e09f9a7d839c17539808c231f8536
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-02-07 18:06:05 -0300

    Corrigindo bugs

---
+++ modulo 3 - programacao orientada a objetos/VPL 4 - Decorator/OpacidadeDecorator.cpp
@@ -0,0 +1,21 @@
+#include "OpacidadeDecorator.hpp"
+
+std::string OpacidadeDecorator::desenha() {
+    if(this->_opacidade >= 0 && this->_opacidade <= 0.33){
+        return FormaDecorator::desenha() + "\n- opacidade: baixa"; 
+    }
+    if(this->_opacidade > 0.33 && this->_opacidade <= 0.66){
+        return FormaDecorator::desenha() + "\n- opacidade: média";
+    }
+    if(this->_opacidade > 0.66 && this->_opacidade <= 1){
+        return FormaDecorator::desenha() + "\n- opacidade: alta";
+    }
+}
+
+void OpacidadeDecorator::setOpacidade(double opacidade) {
+    this->_opacidade = opacidade;
+}
+
+double OpacidadeDecorator::getOpacidade() {
+    return this->_opacidade;
+}
