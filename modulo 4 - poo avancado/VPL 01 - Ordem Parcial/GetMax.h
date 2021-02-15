a0db003910e1e6e3ebb6f2e032e3e7c6cd3c7f9a
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-02-15 20:51:41 -0300

    VPL 01 finalizado

--- modulo 4 - poo avancado/VPL 01 - Ordem Parcial/GetMax.h
+++ modulo 4 - poo avancado/VPL 01 - Ordem Parcial/GetMax.h
@@ -11,24 +11,25 @@
  * the set <--||----------------------------> is the binary representation of
  * the number 12, and it means that the elements 3 and 4 are in the set.
  */
-struct BitSet {
-  BitSet(unsigned value): _set(value) {}
-  const unsigned _set;
-  friend std::ostream & operator<<(std::ostream& os, const BitSet& i) {
-    const int limit = sizeof(unsigned) * 8;
-    os << '<';
-    for (int aux = 0; aux < limit; aux++) {
-      unsigned mask = 1 << aux;
-      if (i._set & mask) {
-        os << "|";
-      } else {
-        os << "-";
-      }
+struct BitSet{
+    BitSet(unsigned value) : _set(value) {}
+    const unsigned _set;
+    friend std::ostream &operator<<(std::ostream &os, const BitSet &i){
+        const int limit = sizeof(unsigned) * 8;
+        os << '<';
+        for (int aux = 0; aux < limit; aux++){
+            unsigned mask = 1 << aux;
+            if (i._set & mask){
+                os << "|";
+            }
+            else{
+                os << "-";
+            }
+        }
+        os << '>';
+        return os;
     }
-    os << '>';
-    return os;
-  }
-  /**
+    /**
    * \brief compares two bit sets. A bit set b1 is greater than or equal to
    * another bit set b2 if b1 contains all the bits in b2. For instance, the
    * bit set represented by 15 is greater than the bit set represented by 12.
@@ -36,7 +37,15 @@ struct BitSet {
    * former does not contain the first element, which is in the latter.
    * \return true if bit_set1 is greater than bit_set2
    */
-  // TODO: implement this operator.
+
+    bool operator >= (const BitSet& bitSet){
+        if((_set & bitSet._set) == _set){ // & = bitwise AND
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 };
 
 /**
@@ -45,21 +54,29 @@ struct BitSet {
  * the interval (2, 6) contains all the elements from 2 to 6, including these
  * two.
  */
-struct Interval {
-  Interval(int left, int right): _l(left), _r(right) {}
-  const int _l;
-  const int _r;
-  friend std::ostream & operator<<(std::ostream& os, const Interval& i) {
-    os << '(' << i._l << ", " << i._r << ')';
-    return os;
-  }
-  /**
+struct Interval{
+    Interval(int left, int right) : _l(left), _r(right) {}
+    const int _l;
+    const int _r;
+    friend std::ostream &operator<<(std::ostream &os, const Interval &i){
+        os << '(' << i._l << ", " << i._r << ')';
+        return os;
+    }
+    /**
    * \brief compares two intervals to see if the first is greater than or equal
    * the second. An interval (a1, a2) is greater than or equal another interval
    * (b1, b2) if a1 <= b1 and a2 >= b2.
    * \return true if interval1 is greater than or equal interval2
    */
-  // TODO: implement this operator.
+
+    bool operator >= (const Interval& interval){
+        if(_l <= interval._l && _r >= interval._r){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 };
 
 /**
@@ -67,8 +84,16 @@ struct Interval {
  * them is the greatest.
  */
 template <class T>
-T GetMaxDefault (T a, T b, T dflt) {
-  // TODO: implement this generic function.
+T GetMaxDefault(T a, T b, T dflt){
+    if(a >= b){
+        return a;
+    }
+    else if(b >= a){
+        return b;
+    }
+    else{
+        return dflt;
+    }
 }
 
 #endif
