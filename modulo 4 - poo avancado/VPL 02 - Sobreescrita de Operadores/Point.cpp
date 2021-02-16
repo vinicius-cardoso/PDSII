ab116afcfeee4252aafa1ec733da40e98ea1b8e7
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-02-16 00:23:24 -0300

    VPL 02 finalizado

--- modulo 4 - poo avancado/VPL 02 - Sobreescrita de Operadores/Point.cpp
+++ modulo 4 - poo avancado/VPL 02 - Sobreescrita de Operadores/Point.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-
 #include "Point.h"
 
-std::ostream & operator << (std::ostream &out, const Point &p) {
-  // TODO: implement this operator.
+std::ostream &operator << (std::ostream &out, const Point &p){
+    return out << "(" << p.x << ", " << p.y << ")";
 }
 
-std::istream & operator >> (std::istream &in,  Point &p) {
-  // TODO: implement this operator.
+std::istream &operator >> (std::istream &in, Point &p){
+    in >> p.x >> p.y;
+
+    return in;
 }
 
-Point Point::operator + (const Point &p) {
-  // TODO: implement this operator.
+Point Point::operator + (const Point &p){
+    double X = this->x + p.getX();
+    double Y = this->y + p.getY();
+
+    Point ponto = Point(X, Y);
+
+    return ponto; 
 }
 
-Point& Point::operator += (const Point &p) {
-  // TODO: implement this operator.
+Point &Point::operator += (const Point &p){
+    double X = this->x + p.getX();
+    double Y = this->y + p.getY();
+
+    this->x = X;
+    this->y = Y;
+
+    return *this;
 }
--- modulo 4 - poo avancado/VPL 02 - Sobreescrita de Operadores/main.cpp
+++ modulo 4 - poo avancado/VPL 02 - Sobreescrita de Operadores/main.cpp
@@ -1,47 +1,48 @@
 #include <vector>
 #include <iostream>
-
 #include "Point.h"
 
-void readPoints(std::vector<Point> &vec) {
-  Point p;
-  while (std::cin >> p) {
-    vec.push_back(p);
-  }
+void readPoints(std::vector<Point> &vec){
+    Point p;
+    while (std::cin >> p)
+    {
+        vec.push_back(p);
+    }
 }
 
-void printPoints(std::vector<Point> &vec) {
-  for (Point p: vec) {
-    std::cout << p << " ";
-  }
-  std::cout << std::endl;
+void printPoints(std::vector<Point> &vec){
+    for (Point p : vec){
+        std::cout << p << " ";
+    }
+    std::cout << std::endl;
 }
 
-Point sumAssPoints(std::vector<Point> &vec) {
-  Point pSum;
-  for (Point p: vec) {
-    pSum += p;
-  }
-  return pSum;
+Point sumAssPoints(std::vector<Point> &vec){
+    Point pSum;
+    for (Point p : vec){
+        pSum += p;
+    }
+    return pSum;
 }
 
-Point sumBinPoints(std::vector<Point> &vec) {
-  Point pSum;
-  for (Point p: vec) {
-    pSum = pSum + p;
-  }
-  return pSum;
+Point sumBinPoints(std::vector<Point> &vec){
+    Point pSum;
+    for (Point p : vec){
+        pSum = pSum + p;
+    }
+    return pSum;
 }
 
-int main() {
-  std::vector<Point> vec;
-  char testCase;
-  std::cin >> testCase;
-  readPoints(vec);
-  if (testCase == 'a') {
-    std::cout << "Sum = " << sumAssPoints(vec) << std::endl;
-  } else if (testCase == 'b') {
-    std::cout << "Sum = " << sumBinPoints(vec) << std::endl;
-  }
-  printPoints(vec);
+int main(){
+    std::vector<Point> vec;
+    char testCase;
+    std::cin >> testCase;
+    readPoints(vec);
+    if (testCase == 'a'){
+        std::cout << "Sum = " << sumAssPoints(vec) << std::endl;
+    }
+    else if (testCase == 'b'){
+        std::cout << "Sum = " << sumBinPoints(vec) << std::endl;
+    }
+    printPoints(vec);
 }
