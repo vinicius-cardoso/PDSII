9ce2f1ecae5a96f84084085722c5067adc8cccc5
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-03-08 18:40:35 -0300

    Modulo 5 - Boas Práticas B

---
+++ modulo 5 - boas praticas b/src/automovel/Ferrari.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Ferrari.hpp"
+
+void Carro::Ferrari::start() {
+  std::cout << "Starting a Ferrari\n";
+}
+
+void Carro::Ferrari::drive() {
+  std::cout << "Driving a Ferrari\n";
+}
+
+void Carro::Ferrari::stop() {
+  std::cout << "Stopping a Ferrari\n";
+}
---
+++ modulo 5 - boas praticas b/src/automovel/Ford.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Ford.hpp"
+
+namespace Carro {
+
+    void Ford::start() {
+      std::cout << "Starting a Ford\n";
+    }
+
+    void Ford::drive() {
+      std::cout << "Driving a Ford\n";
+    }
+
+    void Ford::stop() {
+      std::cout << "Stopping a Ford\n";
+    }
+
+}
