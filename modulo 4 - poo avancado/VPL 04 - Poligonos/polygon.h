84db04c27c775af18d3b5aa22f10effa90f259ae
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-03-08 17:22:43 -0300

    VPL 04 - não finalizado

--- modulo 4 - poo avancado/VPL 04 - Poligonos/polygon.h
+++ modulo 4 - poo avancado/VPL 04 - Poligonos/polygon.h
@@ -22,8 +22,9 @@ class Point;
  * these points are organized. Nevertheless, every instance of Polygon must
  * know how to be converted into a vector of points.
  */
-class Polygon {
-  public:
+class Polygon{
+
+public:
     // The virtual destructor, because this class is abstract.
     virtual ~Polygon() {}
     /**
@@ -31,17 +32,18 @@ class Polygon {
      * \param a point that we verify if is contained in the polygon.
      * \return True if the polygon contains p
      */
-    virtual bool contains(const Point& p) const = 0;
+    virtual bool contains(const Point &p) const = 0;
     /**
      * \brief Convertion of a polygon into a vector of points.
      */
-    operator const std::vector<Point>&() const { return limits; }
+    operator const std::vector<Point> &() const { return limits; }
     /**
      * \brief Streaming operator that defines how the polygon must be printed
      * into some output.
      */
-    friend std::ostream& operator << (std::ostream &out, const Polygon &poly);
-  protected:
+    friend std::ostream &operator << (std::ostream &out, const Polygon &poly);
+
+protected:
     /**
      * \brief The list of points that make up the polygon. The important
      * contract of this data structure is that the polygon contains an edge
@@ -63,8 +65,9 @@ bool operator == (const Polygon &lhs, const Polygon &rhs);
  *
  * \brief a Point is the simplest type of polygon.
  */
-class Point: public Polygon {
-  public:
+class Point : public Polygon{
+
+public:
     /**
      * \brief The constructor creates a point with coordenates xx and yy.
      */
@@ -74,11 +77,11 @@ class Point: public Polygon {
      * \return true if these two points are the same, i.e., have the same
      * coordinates.
      */
-    bool contains(const Point& p) const;
+    bool contains(const Point &p) const;
     /**
      * \brief prints the point into the stream as a pair (xx, yy)
      */
-    friend std::ostream& operator << (std::ostream &out, const Point &p);
+    friend std::ostream &operator<<(std::ostream &out, const Point &p);
 
     const int x; ///< The X coordinate of the point
     const int y; ///< The Y coordinate of the point
@@ -91,8 +94,9 @@ class Point: public Polygon {
  * points.
  * The edges of this rectangle are parallel with the carthesian axes.
  */
-class RightRectangle: public Polygon {
-  public:
+class RightRectangle : public Polygon{
+
+public:
     /**
      * \brief the constructor that creates a rectangle bounded by x0, x1, y0
      * and y1.
@@ -103,23 +107,22 @@ class RightRectangle: public Polygon {
      * \return true if the coordinates of p lay inside the bounds x0, x1, y0
      * and y1
      */
-    bool contains(const Point& p) const;
+    bool contains(const Point &p) const;
 };
 
 /**
  * A right square is a particular kind of right rectangle. It's sides have
  * equal lengths.
  */
-class RightSquare: public RightRectangle {
-  public:
+class RightSquare : public RightRectangle{
+
+public:
     /**
      * \brief the constructor that will create a Right rectantle with lower
      * left corner at (botLeftXX, botLeftYY), and upper right corner at
      * (botLeftXX + side, botLeftYY + side)
      */
-    RightSquare(int botLeftXX, int botLeftYY, double side):
-      RightRectangle(botLeftXX, botLeftYY, botLeftXX + side, botLeftYY + side) {
-    }
+    RightSquare(int botLeftXX, int botLeftYY, double side) : RightRectangle(botLeftXX, botLeftYY, botLeftXX + side, botLeftYY + side){}
 };
 
 #endif
