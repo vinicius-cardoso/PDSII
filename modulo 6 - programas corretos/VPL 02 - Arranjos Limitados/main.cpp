3e966e685b45c5fb8667c78d9e17869f26ba16e2
vinicius-cardoso <vinicius.mct17@gmail.com>
2021-03-15 21:12:50 -0300

    VPL 02 pronto

--- modulo 6 - programas corretos/VPL 02 - Arranjos Limitados/main.cpp
+++ modulo 6 - programas corretos/VPL 02 - Arranjos Limitados/main.cpp
@@ -1,50 +1,78 @@
 #include <iostream>
 
-template <class T, int N> class BoundedArray {
-  public:
-    void set(int index, T value) {
-      buf[index] = value;
+class IndiceNaoInicializado {};
+class IndiceNegativo {};
+class IndiceMaiorQueArranjo {};
+
+template <class T, int N> class BoundedArray{
+
+public:
+    BoundedArray(){
+        for(int i = 0; i < N; i++){
+            index_ref[i] = false;
+        }
     }
-    T get(int index) {
-      return buf[index];
+
+    void set(int index, T value){
+        if(index >= N) throw IndiceMaiorQueArranjo();
+        if(index < 0) throw IndiceNegativo();
+
+        index_ref[index] = true;
+        buf[index] = value;
     }
-  private:
+    T get(int index){
+        if(index >= N) throw IndiceMaiorQueArranjo();
+        if(index < 0) throw IndiceNegativo();
+        if(index_ref[index] == false) throw IndiceNaoInicializado();
+
+        return buf[index];
+    }
+
+private:
     T buf[N];
+    bool index_ref[N];
 };
 
-template <class T> void testArray() {
-  BoundedArray<T, 8> a;
-  char action;
-  while (std::cin >> action) {
-    int index;
-    std::cin >> index;
-    try {
-      if (action == 's') {
-        T value;
-        std::cin >> value;
-        a.set(index, value);
-      } else if (action == 'g') {
-        std::cout << a.get(index) << std::endl;
-      }
-    } catch (...) {
-      std::cerr << "Erro desconhecido." << std::endl;
+template <class T>
+void testArray(){
+    BoundedArray<T, 8> a;
+    char action;
+    while (std::cin >> action){
+        int index;
+        std::cin >> index;
+        try{
+            if (action == 's'){
+                T value;
+                std::cin >> value;
+                a.set(index, value);
+            }
+            else if (action == 'g'){
+                std::cout << a.get(index) << std::endl;
+            }
+        } catch (IndiceNaoInicializado){
+            std::cerr << "Erro: indice nao inicializado." << std::endl;
+        } catch (IndiceNegativo){
+            std::cerr << "Erro: indice negativo." << std::endl;
+        } catch (IndiceMaiorQueArranjo){
+            std::cerr << "Erro: indice maior que arranjo." << std::endl;
+        }
+
     }
-  }
 }
 
-int main() {
-  char type;
-  std::cin >> type;
-  switch(type) {
+int main(){
+    char type;
+    std::cin >> type;
+    switch (type){
     case 'd':
-      testArray<double>();
-      break;
+        testArray<double>();
+        break;
     case 'i':
-      testArray<int>();
-      break;
+        testArray<int>();
+        break;
     case 's':
-      testArray<std::string>();
-      break;
-  }
-  return 0;
+        testArray<std::string>();
+        break;
+    }
+    return 0;
 }
